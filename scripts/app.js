ddee5d9bf0b0661b7f7965c7d903c527ffbc11fd
Victor <luanvitorlima2017@gmail.com>
2024-03-02 17:04:15 -0300

    alteracao no js

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+
+window.onload=function(){
+    const hamburguer = document.querySelector(".hamburguer");
+    const nav = document.querySelector(".nav");
+    hamburguer.addEventListener("click", () => 
+    nav.classList.toggle("active"));
+}
